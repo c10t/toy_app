487cf797bcaee0a5559a077842d18572df5a66f7
c10t <rem_tate@yahoo.co.jp>
2017-06-20 18:41:11 +0000

    not to show first post

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -65,7 +65,6 @@ class UsersController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_user
       @user = User.find(params[:id])
-      @first_post = @user.microposts.first
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
